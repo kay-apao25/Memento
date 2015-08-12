e900826dab11533f0a225d10e7b616a071f774e1
kay-apao25 <kayapao@yahoo.com>
2015-08-12 20:50:12 +0800

    [TASK][WIP] Index - Enable search form upon hovering logo

---
+++ javascript/search.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("300");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("300");
+            $(this).toggleClass('open');       
+        }
+    );
+});
